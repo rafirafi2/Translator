ec2d62e905c24e2c285a474741244e5fd45d3afd
Rafi <rafirafi9550@gmail.com>
2023-10-28 20:23:00 -0400

    added spring

---
+++ translate-spring/translate-spring/src/main/java/com/example/translatespring/TranslateSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.translatespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslateSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslateSpringApplication.class, args);
+	}
+
+}
---
+++ translate-spring/translate-spring/src/test/java/com/example/translatespring/TranslateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.translatespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslateSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
